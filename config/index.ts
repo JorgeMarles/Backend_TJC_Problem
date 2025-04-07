c117e43801fab02f17a0b701abe2a84d87a4d445
Juan Afanador <juanafanador07@gmail.com>
2025-04-06 19:49:06 -0500

    chore: Remove unused vars

--- config/index.ts
+++ config/index.ts
@@ -8,7 +8,6 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || "";
 export const DB_HOST = process.env.DB_HOST || "";
 export const DB_NAME = process.env.DB_NAME || "";
 export const DB_PORT = process.env.DB_PORT || "";
-export const TABLE_NAMES = process.env.TABLE_NAMES || "";
 export const URL_FRONTEND = process.env.URL_FRONTEND || "";
 export const URL_RUNNER = process.env.URL_RUNNER || "";
 export const URL_PUBLIC_KEY = process.env.URL_PUBLIC_KEY || "";
