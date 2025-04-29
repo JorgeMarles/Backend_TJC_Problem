c444d84b43518b0e50b00c8919147a10cbf90b24
JorgeMarles <jamarlesf@gmail.com>
2025-04-29 11:01:19 -0500

    fix: Problem search now returns multiple results

--- repositories/ProblemRepository.ts
+++ repositories/ProblemRepository.ts
@@ -2,11 +2,11 @@ import { AppDataSource } from "../database";
 import { Problem } from "../database/entity/Problem";
 
 export const ProblemRepository = AppDataSource.getRepository(Problem).extend({
-    async findBySearch  (query: string): Promise<Problem | null> {
+    async findBySearch  (query: string): Promise<Problem[]> {
         return this.createQueryBuilder("problem")
             .where("LOWER(problem.name) LIKE :query", { query: `%${query}%` })
             .andWhere("problem.disable = false") // Filtra por problemas habilitados
             .leftJoinAndSelect("problem.topic", "topic")
-            .getOne(); // Devuelve los resultados como objetos planos
+            .getMany(); // Devuelve los resultados como objetos planos
     }
 });
